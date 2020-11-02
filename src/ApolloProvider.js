dad1511f51b968fe4222439e9b4dcd584eafe8be
Szabo Vilmos <vili8899s@gmail.com>
2020-11-02 19:49:47 +0100

    changed apolloProvider wsLink

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -31,7 +31,7 @@ httpLink = authLink.concat(httpLink)
 const host = window.location.host
 
 const wsLink = new WebSocketLink({
-  uri: `https://node-mysql-graphql-chat.herokuapp.com/`,
+  uri: `wss://node-mysql-graphql-chat.herokuapp.com/`,
   options: {
     reconnect: true,
     connectionParams: {
